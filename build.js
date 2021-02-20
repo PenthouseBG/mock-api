786b7a7c600e46f3c00133415ac063717a1d7be0
nelfurion <angelovradostin@gmail.com>
2021-02-21 01:36:04 +0200

    adding apartment searches

---
+++ build.js
@@ -0,0 +1,14 @@
+const jsonfile = require('jsonfile')
+const jsonMerger = require('json-merger')
+const fs = require('fs')
+
+const result = jsonMerger.mergeFiles([
+  'config.json', 
+  'users.json',
+  'apartments.json',
+  'apartmentSearches.json'
+])
+
+jsonfile.writeFile('db.json', result, { spaces: 2 }, (err, result) => {
+  if (err) { console.error(err) }
+})
